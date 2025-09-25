ec6dea778cff920e89009f2a493e2e461617befa
Samuel Sampaio <samukasmk@gmail.com>
2025-09-25 13:06:58 -0300

    fix create cluster and install ingress

--- 1.cluster/create-cluster.sh
+++ 1.cluster/create-cluster.sh
@@ -1,2 +1,14 @@
-echo "# kind create cluster --config ./manifests/kind.yml"
-kind create cluster --config ./manifests/kind.yml
+#!/bin/bash
+#
+CLUSTER_NAME=$1
+if [ -z "$CLUSTER_NAME" ]; then
+	echo "Error: Cluster name is undefined. Please define a cluster name."
+	echo "Example: $0 <cluster_name>"
+	exit 1
+fi
+
+echo '=========================================================================='
+echo "# kind create cluster --name \"$CLUSTER_NAME\" --config ./manifests/kind.yml"
+echo '=========================================================================='
+kind create cluster --name "$CLUSTER_NAME" --config ./manifests/kind.yml
+
--- 2.ingress/install-ingress.sh
+++ 2.ingress/install-ingress.sh
@@ -1,22 +1,25 @@
+echo '=========================================================================='
 echo "# helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx"
+echo '=========================================================================='
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 
-echo '--------------------------------------------------------------------------'
 echo
-
+echo
+echo '=========================================================================='
 echo "# helm repo update"
+echo '=========================================================================='
 helm repo update
 
-echo '--------------------------------------------------------------------------'
 echo
-
-echo "helm install ingress-nginx ingress-nginx/ingress-nginx"
+echo
+echo '=========================================================================='
+echo "# helm install ingress-nginx ingress-nginx/ingress-nginx \\"
 echo "  --namespace ingress-nginx --create-namespace \\"
 echo "  --set controller.kind=DaemonSet \\"
 echo "  --set controller.hostPort.enabled=true \\"
 echo "  --set-string controller.nodeSelector.ingress-ready=true \\"
 echo "  --set defaultBackend.enabled=true"
-
+echo '=========================================================================='
 helm install ingress-nginx ingress-nginx/ingress-nginx \
   --namespace ingress-nginx --create-namespace \
   --set controller.kind=DaemonSet \
@@ -24,4 +27,3 @@ helm install ingress-nginx ingress-nginx/ingress-nginx \
   --set-string controller.nodeSelector.ingress-ready=true \
   --set defaultBackend.enabled=true
 
-echo '--------------------------------------------------------------------------'
